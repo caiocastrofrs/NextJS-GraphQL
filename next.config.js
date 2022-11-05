ca1bb87f05f9c966999cd9edbd4e118ce1259198
Caio <caiocastrofrs@gmail.com>
2022-11-05 17:36:35 -0300

    finalizado o boilerplate do projeto - aula 20

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+
+module.exports = withPWA({
+  nextConfig
+})
+
 
 module.exports = nextConfig
